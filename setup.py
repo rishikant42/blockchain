883c2621f357ae6a1f76729272179b4322463805
rishikant42 <rksbtp@gmail.com>
2018-06-15 12:42:17 +0530

    added python pkg installer

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(name='blockchainpy',
+      version='0.0.1',
+      description='A blockchain implementation in Python',
+      url='https://github.com/rishikant42/blockchain',
+      author='Rishi kant',
+      author_email='rshkntshrm@gmail.com',
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      license='MIT',
+      packages=['blockchain'],
+      install_requires=[
+          'pycrypto',
+      ],
+      zip_safe=False)
