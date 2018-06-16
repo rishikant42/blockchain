a874e93c4b2157534646320a558eefcc355d8c85
rishikant42 <rksbtp@gmail.com>
2018-06-16 16:54:12 +0530

    removed balance from wallet

--- blockchain/client.py
+++ blockchain/client.py
@@ -1,7 +1,6 @@
 from binascii import hexlify, unhexlify
 from collections import OrderedDict
 
-import Crypto
 from Crypto import Random
 from Crypto.Hash import SHA
 from Crypto.PublicKey import RSA
@@ -9,8 +8,8 @@ from Crypto.Signature import PKCS1_v1_5
 
 
 class Wallet:
-    def __init__(self):
-        self.balance = 1
+    #def __init__(self):
+    #    self.balance = 1
 
     def new_wallet(self):
         random_generator = Random.new().read
@@ -22,7 +21,7 @@ class Wallet:
         response = {
             'private_key': hexlify(private_key).decode('ascii'),
             'address': hexlify(public_key).decode('ascii'),
-            'balance': self.balance,
+            #'balance': self.balance,
         }
         return response
 
